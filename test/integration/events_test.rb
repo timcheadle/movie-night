0061b44ddd306e8990e6c3b106eb97cc4522b369
Tim Cheadle <tim@rationalmeans.com>
2015-09-25 15:45:01 -0400

    Added tests for Event update/delete

--- test/integration/events_test.rb
+++ test/integration/events_test.rb
@@ -44,4 +44,37 @@ class EventsTest < ActionDispatch::IntegrationTest
     assert page.has_content?('Industry')
     assert page.has_content?(event_time)
   end
+
+  test 'Can update events' do
+    event_time = 20.days.from_now
+
+    january = events(:january)
+
+    visit events_path
+
+    click_link january.location
+    click_link 'Edit Event'
+
+    fill_in 'Location', with: 'New Place'
+    fill_in 'Date/Time', with: event_time
+
+    click_button 'Update Event'
+
+    assert page.has_content?('New Place')
+    assert page.has_content?(event_time)
+  end
+
+  test 'Can delete events' do
+    january = events(:january)
+
+    visit events_path
+
+    click_link january.location
+    click_link 'Delete Event'
+
+    assert_equal events_path, current_path
+
+    refute page.has_content?(january.location)
+    refute page.has_content?(january.occurs_at)
+  end
 end
