5d052a6588d5313e45385016922046de9b7652a5
Tim Cheadle <tim@rationalmeans.com>
2015-09-29 15:22:10 -0400

    Added Event controller create/edit/update/destroy

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,6 +12,7 @@ class EventsController < ApplicationController
   end
 
   def edit
+    @event = Event.find(params[:id])
   end
 
   def create
@@ -25,9 +26,20 @@ class EventsController < ApplicationController
   end
 
   def update
+    @event = Event.find(params[:id])
+
+    if @event.update(event_params)
+      redirect_to @event, notice: 'Event created successfully'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @event = Event.find(params[:id])
+
+    @event.destroy
+    redirect_to events_url, notice: 'Event deleted successfully'
   end
 
   private
