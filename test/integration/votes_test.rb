a3969977f5a4279f52a8c072c307d72352b2b479
Tim Cheadle <tim@rationalmeans.com>
2015-10-13 18:10:28 -0400

    Added Voting UI and tests

---
+++ test/integration/votes_test.rb
@@ -0,0 +1,25 @@
+require "test_helper"
+
+class VotesTest < ActionDispatch::IntegrationTest
+  test "Event page shows how many votes each movie has" do
+    january = events(:january)
+
+    visit event_path(january)
+
+    assert find("li", text: "Alien").has_content?("Votes: 2")
+    assert find("li", text: "Tron").has_content?("Votes: 1")
+  end
+
+  test "Event page allows you to vote for a movie" do
+    january = events(:january)
+    alien = movies(:alien)
+
+    visit event_path(january)
+
+    movie_row = find("li", text: alien.title)
+    movie_row.click_link("Vote")
+
+    assert_equal 3, alien.votes.count
+    assert movie_row.has_content?("Votes: 3")
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,8 @@ require 'rails/test_help'
 require 'minitest/reporters'
 require "capybara/rails"
 
-Minitest::Reporters.use!
+reporter_options = { color: true }
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options)]
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
