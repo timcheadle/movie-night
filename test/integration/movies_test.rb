a1b824bd35f6d9f0b262b414320c5076555b3e7d
Tim Cheadle <tim@rationalmeans.com>
2015-10-27 16:19:17 -0400

    Updated Movies integration test to use Devise

--- test/integration/movies_test.rb
+++ test/integration/movies_test.rb
@@ -1,22 +1,27 @@
 require 'test_helper'
 
 class MoviesTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:alice)
+    sign_in(@user)
+
+    @january = events(:january)
+    visit event_path(@january)
+  end
+
+  def teardown
+    Warden.test_reset!
+  end
+
   test 'Event page shows all movies' do
-    january = events(:january)
     alien = movies(:alien)
     tron  = movies(:tron)
-
-    visit event_path(january)
 
     assert page.has_link?(alien.title, href: alien.url)
     assert page.has_link?(tron.title, href: tron.url)
   end
 
   test "Event page allows you to suggest a movie" do
-    january = events(:january)
-
-    visit event_path(january)
-
     within("#new_movie") do
       fill_in "Title", with: "Star Wars"
       fill_in "URL", with: "http://example.com"
@@ -27,10 +32,6 @@ class MoviesTest < ActionDispatch::IntegrationTest
   end
 
   test "Event page doesn't allow you suggest an invalid movie" do
-    january = events(:january)
-
-    visit event_path(january)
-
     within("#new_movie") do
       click_button "Suggest Movie"
     end
@@ -40,12 +41,9 @@ class MoviesTest < ActionDispatch::IntegrationTest
   end
 
   test "Event page allows you delete a movie" do
-    january = events(:january)
     alien = movies(:alien)
 
-    visit event_path(january)
-
-    find("li", text: alien.title).click_link("Delete")
+    find("tr", text: alien.title).click_link("Delete")
 
     refute page.has_link?(alien.title)
   end
