dbac77b3769835c47e7113c786a288b553f447e4
Tim Cheadle <tim@rationalmeans.com>
2015-10-27 16:18:36 -0400

    Added Devise test helper support, fixed User fixtures

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,5 +14,15 @@ class ActiveSupport::TestCase
 end
 
 class ActionDispatch::IntegrationTest
-    include Capybara::DSL
+  include Capybara::DSL
+  include Warden::Test::Helpers
+  Warden.test_mode!
+
+  def sign_in(user)
+    login_as(user, scope: :user)
+  end
+
+  def sign_out
+    logout(:user)
+  end
 end
